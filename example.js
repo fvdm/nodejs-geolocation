18a8175138cfa4ebb0f3551541cd4f39e2cd46f1
Franklin <fr@nkl.in>
2024-03-29 10:43:19 +0100

    Chore(package): Added example.js

---
+++ example.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+const geo = require( 'google-geolocation' );
+
+function out( obj ) {
+  console.dir( obj, {
+    depth: null,
+    colors: true,
+  } );
+}
+
+// Get data
+geo( {
+  key: 'abc123',
+  wifiAccessPoints: [
+    {
+      macAddress: '01:23:45:67:89:AB',
+      signalStrength: -65,
+      signalToNoiseRatio: 40,
+    }
+  ],
+} )
+  .then( out )
+  .catch( err => {
+    out( err );
+    process.exit( 1 );
+  } )
+;
